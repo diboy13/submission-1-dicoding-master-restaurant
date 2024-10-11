566e52e4ca1affb8c98bf09306ae0f11804b6bd6
diboy13 <hadidicky31@gmail.com>
2024-10-11 21:54:54 +0700

    update ui

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,15 +7,38 @@ console.log(menu.menus);
 
 const articles = document.querySelector(".articles");
 const menus = document.querySelector(".menus");
+const navbar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const toTop = document.querySelector(".to-top");
+  const fixedNavbar = header.offsetTop;
+
+  if (window.scrollY > fixedNavbar) {
+    toTop.classList.add("active");
+    toTop.classList.remove("hidden");
+  } else {
+    toTop.classList.add("hidden");
+    toTop.classList.remove("active");
+  }
+};
 
 const hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
+  navbar.classList.toggle("active");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navbar) {
+    hamburger.classList.remove("hamburger-active");
+    navbar.classList.remove("active");
+  }
 });
 
 menu.menus.forEach((menu) => {
   menus.innerHTML += `
-<div class="card-menu">
+<div class="card-menu" tabindex="0">
     <div class="card-image">
       <img src="${menu.img}">
     </div>
@@ -30,7 +53,7 @@ menu.menus.forEach((menu) => {
 
 data.restaurants.forEach((restaurant) => {
   articles.innerHTML += `
-    <article class="card-restaurant">
+    <article class="card-restaurant" tabindex="0">
       <div class="article-wrapper">
         <figure>
           <img src="${restaurant.pictureId}" alt="" />
@@ -48,7 +71,5 @@ data.restaurants.forEach((restaurant) => {
     </article>
 `;
 });
-
-
 
 console.log("Hello Coders! :)");
